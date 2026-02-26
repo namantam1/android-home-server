dd93178b3ff72f7b12899b1503df65a1d73fa0fa
Naman Tamrakar <namantam1@gmail.com>
2026-02-26 18:02:27 +0530

    fix tunnel url script

--- scripts/cloudflare-tunnel/tunnel-url.sh
+++ scripts/cloudflare-tunnel/tunnel-url.sh
@@ -1,10 +1,12 @@
-METRICS_PORT=4040
+METRICS_PORT=20242
 until curl -s "http://127.0.0.1:${METRICS_PORT}/metrics" \
       | sed -n 's/.*userHostname="\([^"]*\)".*/\1/p' \
       | grep -q 'trycloudflare.com'; do
   sleep 0.2
 done
+TEMP_DIR=$(mktemp -d)
 curl -s "http://127.0.0.1:${METRICS_PORT}/metrics" \
   | sed -n 's/.*userHostname="\([^"]*\)".*/\1/p' \
-  | head -n1 > /tmp/tunnel_url.txt
-echo "Tunnel URL: $(cat /tmp/tunnel_url.txt)"
+  | head -n1 > $TEMP_DIR/tunnel_url.txt
+echo "Tunnel URL: $(cat $TEMP_DIR/tunnel_url.txt)"
+rm -rf TEMP_DIR
